d3e311decb69a368595fd621a134365c805ce73f
ckost9n <ckost9n@gmail.com>
2022-09-23 17:24:38 +0300

    create add and minus card, create card columns

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚲", "🏎", "🚒", "🚌", "🛻", "🚛",
+                  "🛵", "🚜", "🚖", "🚃", "🚘", "🚈",
+                  "✈️", "🚀", "🚁", "🛥", "🚔", "🚕",
+                  "🚑", "🏍", "🚠", "⛵️", "🛸", "🚢"
+    ]
+    @State var emojiCount: Int = 24
+
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in 
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+                .foregroundColor(.red)
+            }
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                Text("Shuffle")
+                    .foregroundColor(.blue)
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            VStack {
+                Image(systemName: "plus.circle")
+                    .resizable()
+                    .frame(width: 30, height: 30, alignment: .center)
+            }
+        }
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            VStack {
+                Image(systemName: "minus.circle")
+                    .resizable()
+                    .frame(width: 30, height: 30, alignment: .center)
+            }
+        }
     }
 }
 
 struct CardView: View {
+    var content: String
     @State var isFaceUp: Bool = false
     
     var body: some View {
@@ -28,8 +78,8 @@ struct CardView: View {
             let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
-                Text("✈️").font(.largeTitle)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
             }
             else {
                 shape.fill()
--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 @main
 struct MemorizeApp: App {
+    
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .onChange(of: scenePhase) { newValue in
+            switch newValue {
+                
+            case .background:
+                print("App is bacground")
+            case .inactive:
+                print("App is inactive")
+            case .active:
+                print("App is active")
+            @unknown default:
+                print("App is error")
+            }
+        }
     }
+    
+        
 }
